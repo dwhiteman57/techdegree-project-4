6f030c57c4fbff0ab95cf2d542b40aaa721f3404
Daniel Whiteman <daniel.whiteman1@gmail.com>
2019-03-12 15:26:06 -0500

    Step 8
    Start Game Function

--- js/app.js
+++ js/app.js
@@ -5,6 +5,18 @@
 
 //Test code
 
-const game = new Game();
-game.startGame();
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+// const game = new Game();
+// game.startGame();
+// console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+
+
+//Step 8: Build Start Game Method
+let game;
+let startButton = document.getElementById('btn__reset');
+
+let startGame = () => {
+  game = new Game();
+  game.startGame();
+}
+
+startButton.addEventListener("click", startGame, false);
